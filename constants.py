02f70867f05d129ca586c3e2c4559e5a1e84d9ec
Aditya Aman <adityaaman22948@gmail.com>
2025-03-04 01:25:36 +0530

    production changes

--- constants.py
+++ constants.py
@@ -1,9 +1,14 @@
 from dotenv import load_dotenv
 import os
-load_dotenv()
 
-SERVER_URL = 'localhost'
-PORT = '8900'
-ENV = 'dev'
+# Load local .env only in development
+if os.getenv("ENV", "dev") == "dev":
+    load_dotenv()
 
+# Get environment variables
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+
+# Server Config
+SERVER_URL = os.getenv("SERVER_URL", "localhost")
+PORT = os.getenv("PORT", "8900")
+ENV = os.getenv("ENV", "dev")  # Default to "dev"
